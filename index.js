d30cffa54a898a287a2a6df273b7b72aa2ac35b2
mdsultanrazibuddin <mdsultanrazibuddin@gmail.com>
2022-05-08 23:27:03 +0600

    added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require ('dotenv').config();
 const port = process.env.PORT || 5000;
 const app = express();
@@ -8,8 +8,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// user = dbuser1
-// password = 9aqvkv4J21mmyHgR
+
+
 
 
 
@@ -27,10 +27,44 @@ async function run(){
         const productCollection = client.db('wareHouse').collection('product');
         
         app.get('/product', async (req, res) =>{
+            const page =parseInt(req.query.page)
+            const size =parseInt(req.query.size)
             const query = {};
             const cursor = productCollection.find(query);
-            const products = await cursor.toArray();
+            let products;
+            if(page || size){
+                products = await cursor.skip(page*size).limit(size).toArray();
+            }
+            else{
+                products = await cursor.toArray();
+            }
+         
             res.send(products);
+        })
+        app.get('/product/:id', async (req, res) =>{
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const product = await productCollection.findOne(query);
+           
+            res.send(product);
+        })
+        app.post('/product', async(req, res) =>{
+            const newProduct = req.body;
+            const result = await productCollection.insertOne(newProduct);
+            res.send(result)
+        })
+        app.delete('/product/:id', async (req, res) =>{
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const result = await productCollection.deleteOne(query);
+           
+            res.send(result);
+        })
+        app.get('/productCount', async(req, res) =>{
+            const query = {};
+            const cursor= productCollection.find(query);
+            const count = await cursor.count();
+            res.send({count}) 
         })
      }
      finally{
